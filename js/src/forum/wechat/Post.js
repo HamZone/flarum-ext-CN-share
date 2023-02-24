be751e3059ffb4c459e9c71df53d342ae3006cbc
emin.lin <emin.lin@live.com>
2023-02-24 15:53:32 +0800

    支持更多页面的转发

--- js/src/forum/wechat/Post.js
+++ js/src/forum/wechat/Post.js
@@ -1,30 +1,27 @@
 import { extend } from 'flarum/common/extend';
-import CommentPost from 'flarum/forum/components/CommentPost';
+// import CommentPost from 'flarum/forum/components/CommentPost';
 import wx from 'weixin-js-sdk-ts'
-import DiscussionHero from 'flarum/forum/components/DiscussionHero';
+import HeaderSecondary from 'flarum/common/components/HeaderSecondary';
 
 export default function () {
-    extend(CommentPost.prototype, 'headerItems', function (items) {
-        const post = this.attrs.post;
-        const id = post.data.relationships.discussion.data.id;
+
+    extend(HeaderSecondary.prototype, 'items', function (items) {
+        console.log(1)
         var ua = window.navigator.userAgent.toLowerCase();
         if (ua.match(/MicroMessenger/i) == 'micromessenger') {
             //微信环境
             wx.miniProgram.getEnv(function (res) {
-                if (res.miniprogram && id) {
-                    app.store.find('discussions', id).then(discussion => {
-                        wx.miniProgram.postMessage({ data: { 
-                            title: discussion.title(), 
-                            path: app.forum.attribute('baseUrl')+"/d/"+id, 
-                            imageUrl: discussion.user().data.attributes.avatarUrl?
-                                discussion.user().data.attributes.avatarUrl:"" 
-                        } })
-                    });
-                   
+                if (res.miniprogram) {
+                    wechat({
+                        title: document.title,
+                        path: window.location.href,
+                    })
                 }
-
             })
-
         }
-    })
+    });
+
+    function wechat(data){
+        wx.miniProgram.postMessage({ data: data })
+    }
 }
