1f16365e95dba11fb8db5e7180b3712fe86cc9e4
emin.lin <emin.lin@live.com>
2023-02-24 16:12:16 +0800

    回滚wx使用方法

--- js/src/forum/wechat/Post.js
+++ js/src/forum/wechat/Post.js
@@ -15,41 +15,32 @@ export default function () {
             wx.miniProgram.getEnv(function (res) {
                 if (res.miniprogram && id) {
                     app.store.find('discussions', id).then(discussion => {
-                        wechat({ 
+                        wx.miniProgram.postMessage({ data:{ 
                             title: discussion.title(), 
                             path: app.forum.attribute('baseUrl')+"/d/"+id, 
                             imageUrl: discussion.user().data.attributes.avatarUrl?
                                 discussion.user().data.attributes.avatarUrl:"" 
-                        })
+                        }})
                     });
                    
                 }
-
             })
-
         }
     })
 
     extend(IndexPage.prototype, 'hero', function (items) {
-        // console.log(app)
-        // console.log(document.title)
-        // console.log(window.location.href)
         var ua = window.navigator.userAgent.toLowerCase();
         if (ua.match(/MicroMessenger/i) == 'micromessenger') {
             //微信环境
             wx.miniProgram.getEnv(function (res) {
                 if (res.miniprogram) {
-                    wechat({
+                    wx.miniProgram.postMessage({ data:{
                         title: document.title,
-                        path:window.location.href,
+                        path: window.location.href,
                         imageUrl: ""
-                    })
+                    }})
                 }
             })
         }
     });
-
-    function wechat(data){
-        wx.miniProgram.postMessage({ data: data })
-    }
 }
